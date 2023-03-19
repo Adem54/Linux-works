65e5473458f6ad42a808551c9dca58d6a03b53ec
Adem Erbas <ae@netsense.no>
2023-03-19 10:49:14 +0100

    test

--- linux-intro/index.php
+++ linux-intro/index.php
@@ -516,5 +516,20 @@ Bir dosyanın çalıştırma iznini iptal etmek için:
 Yukarıdaki -x parametresini kullandığınızda bir dosyanın çalışma iznini iptal edersiniz o dosya tekrar izin verilene kadar çalıştırılamaz.
 */
 
+/*
+…or create a new repository on the command line
+echo "# Linux-works" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Adem54/Linux-works.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/Adem54/Linux-works.git
+git branch -M main
+git push -u origin main
 
+*/
 ?>
+
